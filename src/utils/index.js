6cf4592ffc6c826e4feefd1232745e1a581d6c20
DarlisonBrandao <DarlisonBrandao123@gmail.com>
2025-06-03 16:37:35 -0300

    updates

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,25 @@
-function rotaProtegida(req, res, next) {
-  if (!req.headers['auforization']) {
-    res.send({
+const jwt = require("jsonwebtoken");
+
+function rotaProtegida(req, res, next){
+  const token = req.headers.authorization;
+  if(!token){
+    res.status (401).send({
       tipo: "warning",
       mensagem: "Nao autorizado",
     });
   }
-}  
   
+  jwt.verify(token.split(" ")[1], process.env.SEGREDO, (error) => {
+    if (error){
+      res.status (401).send({
+        tipo: "warning",
+        mensagem: "Token inválido"
+      });
+    }
+    
+    next();
+});  
+}
   module.exports = {
     rotaProtegida,
   };
